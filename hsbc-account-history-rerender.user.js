299b2bc679b47bce39f270e341c2757593b00d6b
Joseph Boiteau <josephboiteau@gmail.com>
2014-11-15 10:44:10 +1100

    jslint + little refactors
    now works under Chromium browser / TamperMonkey

--- hsbc-account-history-rerender.user.js
+++ hsbc-account-history-rerender.user.js
@@ -19,7 +19,7 @@ if(document.getElementsByTagName('title')[0].text.match("Account History")){
     this.css("top", ( $(window).height() - this.height() ) / 2+$(window).scrollTop() + "px");
     this.css("left", ( $(window).width() - this.width() ) / 2+$(window).scrollLeft() + "px");
     return this;
-  }
+  };
 
   // set some style (no need to have wide spaces around lines)
   $('<style type="text/css">table.hsbcTableStyle07 tr td{ padding:0px 5px 0px 5px; line-height: 1em; white-space: nowrap; } </style>').prependTo($('head'));
@@ -32,26 +32,26 @@ if(document.getElementsByTagName('title')[0].text.match("Account History")){
 
   clearTable = function(){
     trs = $('tr', the_table);
-    trs.each(function(index, tr){ if(index!=0 && index!=(trs.length-1)){ $(tr).addClass('to_del'); } });
+    trs.each(function(index, tr){ if(index!==0 && index!=(trs.length-1)){ $(tr).addClass('to_del'); } });
     $('tr.to_del', the_table).remove();
-  }
+  };
 
   Line = function() {
     this.timestamp = undefined;
-  }
+  };
   Line.prototype.populate = function(line_data_array) {
     this.timestamp = line_data_array.timestamp;
     this.details = line_data_array.details;
     this.debit = line_data_array.debit;
     this.credit = line_data_array.credit;
     this.balance = line_data_array.balance;
-  }
+  };
   Line.prototype.setDate = function(dateString) {
     this.timestamp = new Date(dateString).getTime();
-  }
+  };
   Line.prototype.hasDate = function() {
-    return !(this.timestamp == undefined || isNaN(this.timestamp));
-  }
+    return !(this.timestamp === undefined || isNaN(this.timestamp));
+  };
   Line.prototype.getDate = function(){ // reformat the date column
     if(this.hasDate()){
       date = new Date(this.timestamp);
@@ -60,57 +60,57 @@ if(document.getElementsByTagName('title')[0].text.match("Account History")){
     else{
       return undefined;
     }
-  }
+  };
   Line.prototype.setDetails = function(detailsHTML) { // remove some non-useful data from details column and make it 1 line only
     tmp=detailsHTML.split('<br>');
     this.details = this.cleanStrings(tmp[0] + ' - ' + tmp[2] + ' - ' + tmp[3] + ' ' + tmp[4]);
-  }
+  };
   Line.prototype.getDetails = function(){
     return this.details;
-  }
+  };
   Line.prototype.setDebit = function(debit){
     this.debit = this.cleanAmounts(debit);
-  }
+  };
   Line.prototype.getDebit = function(){
     return this.displayDigits(this.emptyIfZero(this.debit));
-  }
+  };
   Line.prototype.setCredit = function(credit){
     this.credit = this.cleanAmounts(credit);
-  }
+  };
   Line.prototype.getCredit = function(){
     return this.displayDigits(this.emptyIfZero(this.credit));
-  }
+  };
   Line.prototype.setBalance = function(balance){
     this.balance = this.cleanAmounts(balance);
-  }
+  };
   Line.prototype.getBalance = function(){
     return this.displayDigits(this.balance);
-  }
+  };
   Line.prototype.cleanAmounts = function(value){
     tmp = parseFloat(this.cleanStrings(value).replace(/[^0-9\.]/g,''));
     if(isNaN(tmp)) return 0;
     else return tmp;
-  }
+  };
   Line.prototype.cleanStrings = function(value){
     return value.replace(/[\t\n\r   ]+/g,' ');
-  }
+  };
   Line.prototype.displayDigits = function(value){
    return value.toString().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,");
-  }
+  };
   Line.prototype.emptyIfZero = function(value){
-    if(value==0) return '&nbsp;';
+    if(value===0) return '&nbsp;';
     else return value+'&nbsp;';
-  }
+  };
 
   History = function(){
     this.lines = [];
-  }
+  };
   History.prototype.add = function(line){
     this.lines.push(line);
-  }
+  };
   History.prototype.toJSON = function(){
     return JSON.stringify(this.lines);
-  }
+  };
   History.prototype.merge = function(anotherHistory){
     var merged = $.merge(this.lines, anotherHistory.lines);
     merged.sort(function(a,b){
@@ -133,12 +133,12 @@ if(document.getElementsByTagName('title')[0].text.match("Account History")){
     }
 
     this.lines = merged;
-  }
+  };
   History.prototype.testMerge = function(){
     console.log('before merge: ' + this.lines.length + 'lines');
     history.merge(this);
     console.log('after merge: ' + this.lines.length + 'lines');
-  }
+  };
   History.prototype.displayAll = function(){
     clearTable();
     last_tr = $('tr', the_table).last(); // the greyed line with sorting arrows at the bottom
@@ -147,7 +147,8 @@ if(document.getElementsByTagName('title')[0].text.match("Account History")){
       var line = new Line();
       line.populate(line_data_array);
       graphData.push([line.timestamp, line.balance]);
-      tr = $('<tr class="hsbcTableRow0'+((index%2==0)?'3':'4')+'">\
+      /*jshint multistr: true */
+      tr = $('<tr class="hsbcTableRow0'+((index%2===0)?'3':'4')+'">\
         <td class="hsbcTableColumn03" headers="header1">'+line.getDate()+'</td>\
         <td class="" headers="header2">'+line.getDetails()+'</td>\
         <td class="hsbcTableColumn03" headers="header3">'+line.getDebit()+'</td>\
@@ -157,18 +158,19 @@ if(document.getElementsByTagName('title')[0].text.match("Account History")){
       </tr>');
       tr.insertBefore(last_tr);
     });
-  }
+  };
 
   var previous_history = new History();
-  var history = new History();
+  var account_history = new History();
 
   // recover the account name
   var account_name = $('#LongSelection1 option[value="'+$('#LongSelection1').val()+'"]').text().replace(/[^a-zA-Z]/g, '');
   console.log('account_name: ', account_name);
 
   // recover existing history
-  if ( previous_history_JSON = localStorage.getItem(account_name) ){
-    previous_history.lines = eval(JSON.parse(previous_history_JSON));
+  previous_history_JSON = localStorage.getItem(account_name);
+  if (previous_history_JSON){
+    previous_history.lines = JSON.parse(previous_history_JSON);
     console.log('previous history from localStorage: ', previous_history.lines.length);
   }
   else{
@@ -176,7 +178,7 @@ if(document.getElementsByTagName('title')[0].text.match("Account History")){
   }
 
   // Drop the right panel containing mostly ads
-  $('.containerRightTools').remove()
+  $('.containerRightTools').remove();
 
   // loop through table lines
   var prev_line = new Line();
@@ -209,40 +211,30 @@ if(document.getElementsByTagName('title')[0].text.match("Account History")){
         td.innerHTML = line.getBalance();
       }
       else if(td.headers=="header6"){ // empty last column
-        td.innerHTML = history.lines.length + 1;
+        td.innerHTML = account_history.lines.length + 1;
       }
     });
 
-    if(line.getDate()!=undefined && line.getBalance()!=undefined){ // the first and last tr of the array are the sorting arrows
-      history.add(line);
+    if(line.getDate()!==undefined && line.getBalance()!==undefined){ // the first and last tr of the array are the sorting arrows
+      account_history.add(line);
     }
 
     prev_line = line;
 
   });
-  console.log('lines parsed: ', history.lines.length);
+  console.log('lines parsed: ', account_history.lines.length);
 
   // merging previous history with actual one and save it locally
-  history.merge(previous_history);
+  //account_history.merge(previous_history);
 
   // saving new merged history to localStorage
-  localStorage.setItem(account_name, JSON.stringify(history.lines));
-  console.log('lines saved after merge: ', history.lines.length);
+  localStorage.setItem(account_name, JSON.stringify(account_history.lines));
+  console.log('lines saved after merge: ', account_history.lines.length);
 
   // rewrite the whole thing
-  history.displayAll();
+  account_history.displayAll();
 
   // 2e part: draw a chart
-  
-  // show/hide functions
-  function show(){
-    $("#chartwrap")
-      .css('display', 'block')
-      .center();
-  }
-  function hide(){
-    $("#chartwrap").css('display', 'none');
-  }
 
   // add a div somewhere in the DOM
   $('<div id="chartwrap" style="border: 1px solid #AAA; background-color:white; position: absolute;"></div>').appendTo("body");
@@ -250,14 +242,22 @@ if(document.getElementsByTagName('title')[0].text.match("Account History")){
 
   // draw the chart in its div
   // TODO: do way better ! watch here: http://people.iola.dk/olau/flot/examples/stacking.html
-  $.plot($("#placeholder"), [graphData], { xaxis: { mode: "time" } });
+  $.plot("#placeholder", [graphData], { xaxis: { mode: "time" } });
+
+  toggleChartDisplay = function(show_or_hide){
+      if($("#chartwrap").is(':hidden')) $("#chartwrap").center();
+      $("#chartwrap").toggle(show_or_hide);
+  };
 
   // display a button to toggle chart visibility
   $('<a class="hsbcLinkStyle06">Display chart</a>')
     .prependTo($('td.hsbcTableColumn03')[0])
     .css('cursor', 'pointer')
-    .toggle(show, hide);
-  $('#placeholder').click(hide);
+    .click(toggleChartDisplay);
 
-  hide();
+  // click on the chart hides it
+  $('#placeholder').click(toggleChartDisplay);
+
+  // start with Chart hidden
+  toggleChartDisplay(false);
 }
